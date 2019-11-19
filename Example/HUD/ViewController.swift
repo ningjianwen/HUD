b6c3070e58326c076dc10e148b542c69f90a23f7
ningjianwen <ningjianwen@yeah.ent>
2019-11-19 23:40:37 +0800

    Initial commit

---
+++ Example/HUD/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HUD
+//
+//  Created by ningjianwen on 11/19/2019.
+//  Copyright (c) 2019 ningjianwen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
